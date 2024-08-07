81dac6649ced9949b79a56349dffefb0f7982696
Ilya <example@gmail.com>
2024-08-07 14:27:19 +0300

    practice 3 part done

--- js/script.js
+++ js/script.js
@@ -141,7 +141,273 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Какой последний фильм вы посмотрели?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Какой последний фильм вы посмотрели?', ''),
+//     d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// 013 Условия !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!')
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Erorr'); //Тернарный оператор
+// 4 + 4 / Бинарный
+// +4 / Унарный
+
+// const num = "50"; // строгое сравнение
+
+// switch (num) {
+//     case 49: 
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case "50":
+//         console.log('В точку!');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+
+// 014 Циклы !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+
+//     console.log(i);
+// 
+
+
+// 015 Практика, ч2. Применяем условия и циклы!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Какой последний фильм вы посмотрели?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// // let i = 0;
+
+// // while (i < 2) {
+// //     const a = prompt('Какой последний фильм вы посмотрели?', ''),
+// //         b = prompt('На сколько оцените его?', '');
+// //     i++;
+
+// //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// //         personalMovieDB.movies[a] = b;
+// //         console.log('done');
+// //     } else {
+// //         console.log('error');
+// //         i--;
+// //     }
+// // }
+
+// // do {
+// //     const a = prompt('Какой последний фильм вы посмотрели?', ''),
+// //         b = prompt('На сколько оцените его?', '');
+// //     i++;
+
+// //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// //         personalMovieDB.movies[a] = b;
+// //         console.log('done');
+// //     } else {
+// //         console.log('error');
+// //         i--;
+// //     }
+// // } while (i < 2);
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+// console.log(personalMovieDB);
+
+
+// 016 Функции, стрелочные ф-ции (ES6)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// let num = 20;
+
+// function showFirstMessage(text) { // function declaration создается до начала выполнения скрипта, можно вызовать перед объявлением
+//     console.log(text);
+//     console.log(num); // идет на уровень выше искать
+// }
+
+// showFirstMessage("hello world!");
+// console.log(num);
+
+// // function calc(a, b) {
+// //     return (a + b); // функция на это заканчивается 
+// // }
+
+// // console.log(calc(4, 3));
+// // console.log(calc(5, 6));
+// // console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {  // function expression создается только тогда, когда доходит поток кода, можно вызвать только после объявления
+//     console.log('Hello');
+// };
+
+// logger();
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// }; //стрелочная функция не имеет своего контекста (this)
+
+// const calc = (a, b) => a + b; // если в одну строку
+
+// const calc = a => a + b; // если имеет один аргумент
+
+//017 Методы и свойства строк и чисел!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// const str = "teSt";
+// const arr = [1, 2, 4];
+
+// // console.log(str[2] = "d");
+
+// console.log(str.toUpperCase()); //метод изменения регистра
+// console.log(str.toLowerCase()); //метод изменения регистра
+// console.log(str);
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit")); //поиск подстроки
+
+// const logg = "Hello world";
+
+// console.log(logg.slice(6)); //метод отрезания части строки
+// console.log(logg.slice(-5, -1));
+
+// console.log(logg.substring(6, 11)); //метод отрезания части строки, не принимает минусовые значения
+
+// console.log(logg.substr(6, 5)); // вторая циферка это  длина сколько символов надо вырезать. Устарела
+
+// const num = 12.2;
+// console.log(Math.round(num)); //округление числа
+
+// const test = "12.2px";
+// console.log(parseInt(test)); //первразает строку в целое число
+// console.log(parseFloat(test)); // первразает строку в дробное число
+
+// 018 Практика , ч3. Используем функции!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -151,24 +417,52 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Какой последний фильм вы посмотрели?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Какой последний фильм вы посмотрели?', ''),
-    d = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Какой последний фильм вы посмотрели?', ''),
+            b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-console.log(personalMovieDB);
+rememberMyFilms();
 
-// let movies = {
-//     name: "",
-//     grade: ""
-// };
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
 
-// movies.name = prompt('Какой последний фильм вы посмотрели?', '');
-// movies.grade = prompt('На сколько оцените его?', '');
+detectPersonalLevel();
 
-// console.log(movies);
+function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    } else {
+        console.log('Error')
+    }
+}
 
+writeYourGenres();
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
+    }
+}
 
